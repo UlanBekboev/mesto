8acbb934655319dac3125b49b9182a78c0554a94
Ulan Bekboev <ubekboouulu@gmail.com>
2022-10-02 00:39:57 +0600

    Bugs are fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
-const popupOpenButton = document.querySelector(".profile__edit-button_action_open");
+const popupOpenButton = document.querySelector(
+  ".profile__edit-button_action_open"
+);
 const popup = document.querySelector(".popup");
 const popupCloseButton = popup.querySelector(".popup__close");
 const formSubmitButton = popup.querySelector(".form__submit-button");
@@ -10,22 +12,17 @@ const job = document.querySelector(".profile__text");
 
 const popupToggle = function (event) {
   popup.classList.toggle("popup_opened");
+  nameInput.value = Name.textContent;
+  jobInput.value = job.textContent;
 };
 popupOpenButton.addEventListener("click", popupToggle);
 popupCloseButton.addEventListener("click", popupToggle);
-
-function formAutoFill() {
-  nameInput.value = Name.textContent;
-  jobInput.value = job.textContent;
-}
-
-popupOpenButton.addEventListener("click", formAutoFill);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   Name.textContent = nameInput.value;
   job.textContent = jobInput.value;
+  popup.classList.remove("popup_opened");
 }
 
 form.addEventListener("submit", formSubmitHandler);
-formSubmitButton.addEventListener("click", popupToggle);
